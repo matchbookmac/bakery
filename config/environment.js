8d843dba5f99bfe4103b7b7f124283674cae191b
kha & Ian MacDonald <student@epicodus.com>
2015-06-24 09:39:24 -0700

    Add content security and bootstrap.

--- config/environment.js
+++ config/environment.js
@@ -18,6 +18,15 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self' 'unsafe-inline' 'unsafe-eval'",
+      'font-src': "'self'",
+      'connect-src': "'self' https://bakerypdx.firebaseio.com/",
+      'img-src': "'self'",
+      'style-src': "'self' 'unsafe-inline'",
+      'frame-src': "'none'"
     }
   };
 

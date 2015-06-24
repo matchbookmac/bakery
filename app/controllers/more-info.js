db61dbb0f25dd045539c6743911d5edc4fbaa18d
Dan Minor & Jesse James <student@epicodus.com>
2015-06-24 16:35:13 -0700

    comment out failed code for records and start parsing dom for checkboxes

--- app/controllers/more-info.js
+++ app/controllers/more-info.js
@@ -4,9 +4,35 @@ needs: ['products', 'contacts']
 
 export default Ember.ArrayController.extend({
   selectedProductsList: null,
+  isChecked: false,
   actions: {
     contactProducts: function() {
       var selectedProducts = this.get('selectedProductsList');
+    },
+    checkboxParseTest: function() {
+      // moving to attempt to parse dom for inputs, gather them, then iterate through them to check for values
+      var checkedBoxes;
+      var checkboxes = document.getElementsByClass('checkbox');
+      checkboxes.forEach(function(box){
+        if (box.checked)
+          checkedBoxes.push(box);
+      });
+      // the above is problematic and relatively untested, will continue on Thurs at 0900
+
+
+      // all comments below are failed experiments
+      // var allValues = this.get('model');
+      // var submittedValues;
+      // allValues.forEach(function(item){
+      //   if (item.isChecked)
+      // })
+      //
+      // for (var i = 0; i <= allValues.length; i++) {
+      //   if (allValues[i].isChecked){
+      //     submittedValues.push(allValues[i]);
+      //   }
+      // }
+      // debugger;
     }
   }
 });

86eff81c8eb1c59755dd5fe24e284e0cc70882da
kha & Ian MacDonald <student@epicodus.com>
2015-06-24 09:37:33 -0700

    Add firebase url

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'bakery',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' wss://*.firebaseio.com" },
+    firebase: 'https://bakerypdx.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {

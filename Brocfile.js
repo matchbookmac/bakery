e65f0ade51db6d2d9272662ad3fa38bc43b1cb75
Cory Spitzer & Kha Le <student@epicodus.com>
2015-06-25 12:22:46 -0700

    Merge and display glyphicon

--- Brocfile.js
+++ Brocfile.js
@@ -17,10 +17,10 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-// app.import('bower_components/bootstrap/dist/js/bootstrap.js');
-// app.import('bower_components/bootstrap/dist/css/bootstrap.css');
-// app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', {
-//   destDir: '/assets'
-// });
+app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', {
+  destDir: '/assets'
+});
 
 module.exports = app.toTree();
